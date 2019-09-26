95358d1a08ab9539f3b5af382e99c69a177a9ec4
muro95 <sonbena95@gmail.com>
2019-09-26 09:18:23 +0700

    first commit

--- src/mai.java
+++ src/mai.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
+
 public class mai {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] str = {"Hello", "Hi", "How", "What", "How"};
+        String str = "Hello World";
         System.out.println("Enter the word you are looking for: ");
-        String guess = input.nextLine();
+        //String guess = input.nextLine();
         int count = 0;
-        boolean isExit = false;
-        for (int i = 0; i < str.length; i ++){
-            if (str[i].equals(guess)){
-                count++;
-                isExit = true;
+        char character = input.next().charAt(0);
+
+        for (int i = 0; i < str.length(); i++) {
+            if (character == str.charAt(i)) {
+                count += 1;
             }
         }
-        if (isExit) {
-            System.out.print("Your word: " + guess + ": " + count + " time");
-        } else {
-            System.out.print("not found");
-        }
+        System.out.println("String has " + count + " " + character);
     }
 }
